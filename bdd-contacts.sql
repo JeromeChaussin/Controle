53aab1d8fb181833cd73ce72e17f5728e0a8daf9
Jérôme Chaussin <jerome.chaussin@aol.com>
2023-11-26 08:41:37 +0100

    Création de la table contacts

--- bdd-contacts.sql
+++ bdd-contacts.sql
@@ -1,10 +1,10 @@
--- Suppression de bdd-contacts si elle existe 
+-- Suppression de exo_contacts si elle existe 
 DROP DATABASE IF EXISTS exo_contacts;
 
--- Création de bdd-contacts si elle n'existe pas
+-- Création de exo_contacts si elle n'existe pas
 CREATE DATABASE IF NOT EXISTS exo_contacts;
 
--- J'utilise la base de donnée bdd-contact
+-- J'utilise la base de donnée exo_contact
 USE exo_contacts;
 
 -- Création de la table des pays
@@ -13,4 +13,18 @@ CREATE TABLE IF NOT EXISTS pays (
     nom VARCHAR(70) NOT NULL,
     iso_2 VARCHAR(2) NOT NULL,
     nationalite VARCHAR(50) NOT NULL
+);
+
+-- Création de la table des contacts
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(70) NOT NULL,
+    prenom VARCHAR(70) NOT NULL,
+    date_de_naissance DATE NOT NULL,
+    sexe VARCHAR(10) NOT NULL,
+    adresse TEXT NOT NULL,
+    cp VARCHAR(10) NOT NULL,
+    ville VARCHAR(70) NOT NULL,
+    pays_iso_3 VARCHAR(3) NOT NULL,
+    FOREIGN KEY (pays_iso_3) REFERENCES pays(iso_3)
 );

d5b83f924860c53b4c0d46607ad84cb4b6cfc5d2
Jérôme Chaussin <jerome.chaussin@aol.com>
2023-11-26 09:22:25 +0100

    Ajout d informations dans la table contacts

--- bdd-contacts.sql
+++ bdd-contacts.sql
@@ -38,6 +38,7 @@ CREATE TABLE IF NOT EXISTS telephone (
     FOREIGN KEY (id_contact) REFERENCES contacts(id)
 );
 
+-- Ajout d'informations dans la table pays
 INSERT INTO pays (iso_3, nom, iso_2, nationalite)
 VALUES
     ('DEU', 'Allemagne', 'DE', 'Allemande'),
@@ -50,3 +51,17 @@ VALUES
     ('AUT', 'Autriche', 'AT', 'Autrichienne'),
     ('CHE', 'Suisse', 'CH', 'Suisse'),
     ('SWE', 'Suède', 'SE', 'Suédoise');
+
+-- Ajout d'informations pour la table contacts
+INSERT INTO contacts (nom, prenom, date_de_naissance, sexe, adresse, cp, ville, pays_iso_3)
+VALUES
+    ('Schmidt', 'Michael', '1995-07-10', 'M', '456 Rue de l Érable', '98765', 'Berlin', 'DEU'),
+    ('Lopez', 'Maria', '1980-12-05', 'F', '123 Rue du Pin', '12345', 'Madrid', 'ESP'),
+    ('Ricci', 'Luca', '1992-09-15', 'M', '789 Rue du Frêne', '54321', 'Rome', 'ITA'),
+    ('Santos', 'Ana', '1984-06-25', 'F', '456 Rue du Bouleau', '34567', 'Londres', 'GBR'),
+    ('Martin', 'Sophie', '1988-03-20', 'F', '789 Rue du Chêne', '75000', 'Paris', 'FRA'),
+    ('Jansen', 'Erik', '1983-04-18', 'M', '123 Rue du Cèdre', '67890', 'Amsterdam', 'NLD'),
+    ('Dubois', 'Camille', '1991-01-30', 'F', '789 Rue du Séquoia', '23456', 'Bruxelles', 'BEL'),
+    ('Müller', 'Andreas', '1986-08-12', 'M', '456 Rue du Sapin', '87654', 'Vienne', 'AUT'),
+    ('Lefevre', 'Juliette', '1990-11-08', 'F', '123 Rue du Peuplier', '65432', 'Genève', 'CHE'),
+    ('Andersson', 'Lars', '1982-02-28', 'M', '789 Rue de l Épicéa', '32109', 'Stockholm', 'SWE');

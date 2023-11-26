9c2af0ba01ec8308bfa803f91e35329395ef7751
Jérôme Chaussin <jerome.chaussin@aol.com>
2023-11-26 08:22:05 +0100

    Création de la table pays

---
+++ bdd-contacts.sql
@@ -0,0 +1,16 @@
+-- Suppression de bdd-contacts si elle existe 
+DROP DATABASE IF EXISTS bdd-contacts;
+
+-- Création de bdd-contacts si elle n'existe pas
+CREATE DATABASE IF NOT EXISTS bdd-contacts;
+
+-- J'utilise la base de donnée bdd-contact
+USE bdd-contacts;
+
+-- Création de la table des pays
+CREATE TABLE IF NOT EXISTS pays (
+    iso_3 VARCHAR(3) PRIMARY KEY,
+    nom VARCHAR(70) NOT NULL,
+    iso_2 VARCHAR(2) NOT NULL,
+    nationalite VARCHAR(50) NOT NULL
+);
